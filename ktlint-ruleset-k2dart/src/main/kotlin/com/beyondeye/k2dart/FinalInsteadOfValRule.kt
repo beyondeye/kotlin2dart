57a3410173e5545260e3b5e7c56513faefbdf375
Dario Elyasy <dario-e@beyond-eye.com>
2022-11-07 10:13:44 +0200

    added UserData to converted node to identify them as Dart code

--- ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/FinalInsteadOfValRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/FinalInsteadOfValRule.kt
@@ -1,5 +1,7 @@
 package com.beyondeye.k2dart
 
+import com.beyondeye.k2dart.utils.asDartNode
+import com.beyondeye.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType.VAR_KEYWORD
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
@@ -12,12 +14,13 @@ public class FinalInsteadOfValRule : Rule("no-val") {
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
+        if(node.isDartNode()) return
         if (node.elementType == VAR_KEYWORD) {
             //*DARIO* important: need also to call emit, that log that we identified something to change in the code
             //        otherwise, the mutated flag will not be set, and the corrected ast will be ignored
             emit(node.startOffset, "final_no_val", true)
-//            (node as LeafPsiElement).rawReplaceWithText("final")
-            (node as LeafElement).replaceWithText("final")
+            val newNode=(node as LeafElement).replaceWithText("final")
+            newNode.asDartNode()
         }
     }
 }
---
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/utils/K2DartUtils.kt
@@ -0,0 +1,16 @@
+package com.beyondeye.k2dart.utils
+
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.com.intellij.openapi.util.Key
+
+internal object K2Dart {
+    val dartNodeKey= Key<Boolean>("*DRT*")
+}
+
+internal fun ASTNode.isDartNode() = getCopyableUserData(K2Dart.dartNodeKey)!=null
+internal fun ASTNode.isNotDartNode() = getCopyableUserData(K2Dart.dartNodeKey)==null
+
+internal fun ASTNode.asDartNode():ASTNode {
+    this.putCopyableUserData(K2Dart.dartNodeKey,true)
+    return this
+}
