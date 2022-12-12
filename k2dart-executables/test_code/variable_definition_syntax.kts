2ccba992f5f3e65ce3ed2c3116661aed88a25b06
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-12 16:03:13 +0200

    added variable_definition_syntax.kts and ast

---
+++ k2dart-executables/test_code/variable_definition_syntax.kts
@@ -0,0 +1,16 @@
+class SomeClass;
+class SomeOtherClass(val a:Double, val b:String)
+
+fun main(arg1:Double, arg2:Float,arg3:AClass) {
+    val sc = SomeClass();
+    val soc = SomeOtherClass(1.0,"one")
+    var sc2:SomeClass = SomeClass()
+    var soc2:SomeOtherClass = SomeOtherClass(2,"two")
+    var d:Double = 1.0
+    var f = 1f
+    var f2:Float = 1f
+    val i:Int = 1
+    val i2  = 1
+    val l:Long= 2L
+    val b:Boolean = true
+}
