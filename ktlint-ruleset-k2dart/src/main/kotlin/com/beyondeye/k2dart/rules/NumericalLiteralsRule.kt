7e3d7f87c8add6323bdfcba78c1da6f493e4391c
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-12 17:57:39 +0200

    added some comments to existing rules

--- ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/rules/NumericalLiteralsRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/rules/NumericalLiteralsRule.kt
@@ -5,7 +5,6 @@ import com.beyondeye.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 
 public class NumericalLiteralsRule : Rule(ruleName) {
