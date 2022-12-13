1f546002b72c26f918032492e075a0ef6083db9a
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-13 12:23:18 +0200

    implementation of VariableTypeBeforeNameRule.kt + test completed

--- ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/rules/VariableTypeBeforeNameRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/rules/VariableTypeBeforeNameRule.kt
@@ -4,10 +4,12 @@ import com.beyondeye.k2dart.utils.asDartNode
 import com.beyondeye.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType
+import com.pinterest.ktlint.core.ast.nextCodeSibling
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 
-//
+
 public class VariableTypeBeforeNameRule : Rule(ruleName) {
     public companion object {
         public const val ruleName:String="basic-types-names"
@@ -18,31 +20,56 @@ public class VariableTypeBeforeNameRule : Rule(ruleName) {
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
         if (node.isDartNode()) return
-        //~.psi.KtTypeReference (TYPE_REFERENCE)
-        //1:               ~.psi.KtUserType (USER_TYPE)
-        //1:                 ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
-        //1:                   ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "Float"
-        if (node.elementType != ElementType.TYPE_REFERENCE) return
-        val identifierNode: ASTNode? = node.firstChildNode.firstChildNode.firstChildNode
-        if (identifierNode?.isDartNode() != false) return
-        if (identifierNode.elementType != ElementType.IDENTIFIER) return
-        identifierNode as LeafPsiElement
-        val identifier = identifierNode.text
-        val newIdentifier: String? =
-            when (identifier) {
-                "Float" -> "double"
-                "Double" -> "double"
-                "Int" -> "int"
-                "Long" -> "int"
-                "Boolean" -> "bool"
-                else -> null
+        //  ~.psi.KtProperty (PROPERTY)
+        //    ~.c.i.p.impl.source.tree.LeafPsiElement (VAR_KEYWORD) "var"
+        //    ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //    ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "d"
+        //    ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
+        //    ~.psi.KtTypeReference (TYPE_REFERENCE)
+        //      ~.psi.KtUserType (USER_TYPE)
+        //        ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
+        //          ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "Double"
+        if(node.elementType != ElementType.PROPERTY) return
+        var varvalKeywordNode=node.firstChildNode
+        var isVar=true
+        if(varvalKeywordNode.elementType==ElementType.VAL_KEYWORD) {
+            isVar=false
+            emit(varvalKeywordNode.startOffset, ruleName, true)
+            varvalKeywordNode = (varvalKeywordNode as LeafPsiElement).replaceWithText("final") //replace "val" with "final"
+            varvalKeywordNode.asDartNode() //set also the dart node flag so that we will avoid processing this node again
+
+        }
+        val identifierNode =varvalKeywordNode.nextCodeSibling()
+        if(identifierNode?.elementType!=ElementType.IDENTIFIER) {
+            //TODO this should not happen
+            return
+        }
+        val potentiallyColonNode=identifierNode.nextCodeSibling()
+        if(potentiallyColonNode?.elementType!=ElementType.COLON) { //we don't have a type specification for the property
+
+        } else { //we have a type specification
+            val typeSpecNode=potentiallyColonNode.nextCodeSibling()
+            if(typeSpecNode?.elementType==ElementType.TYPE_REFERENCE) {
+                emit(node.startOffset, ruleName, true)
+                val parentNode=typeSpecNode.treeParent //todo: we actually have this already as the "node
+                parentNode.removeChild(typeSpecNode)
+                parentNode.addChild(typeSpecNode,identifierNode) //add typespec before identifier
+                parentNode.addChild(PsiWhiteSpaceImpl(" "),identifierNode) //add space between typespec and identifier
+                if(isVar) //remove redundant "var" if this property is not finale
+                {
+                    val spaceAfterValVarKeyword=varvalKeywordNode.treeNext
+                    parentNode.removeChild(varvalKeywordNode)
+                    if(spaceAfterValVarKeyword.elementType==ElementType.WHITE_SPACE) //remove also original white space after var
+                    {
+                        parentNode.removeChild(spaceAfterValVarKeyword)
+                    }
+                }
+                parentNode.removeChild(potentiallyColonNode)
+            } else { //unhandled type definition
+                return
             }
-        newIdentifier?.let {
-            //*DARIO* important: need also to call emit, that log that we identified something to change in the code
-            //        otherwise, the mutated flag will not be set, and the corrected ast will be ignored
-            emit(node.startOffset, ruleName, true)
-            val newIdentifierNode = identifierNode.replaceWithText(newIdentifier) //update the AST with the converted type identifier
-            newIdentifierNode.asDartNode() //set also the dart node flag so that we will avoid processing this node again
+            //TODO this should not happen
+            return
         }
     }
 }
--- ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/VariableTypeBeforeNameRuleTest.kt
+++ ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/VariableTypeBeforeNameRuleTest.kt
@@ -1,6 +1,6 @@
 package com.beyondeye.k2dart
 
-import com.beyondeye.k2dart.rules.BasicTypesNamesRule
+import com.beyondeye.k2dart.rules.VariableTypeBeforeNameRule
 import com.pinterest.ktlint.core.Rule
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
@@ -10,25 +10,55 @@ class VariableTypeBeforeNameRuleTest {
     fun `change basic type names`() {
         val code =
             """
-            fun main(arg1:Double, arg2:Float) {
+            class SomeClass;
+            class SomeOtherClass(val a:Double, val b:String)
+
+            fun main(arg1:Double, arg2:Float,arg3:AClass) {
+                var a:Double
+                val sc = SomeClass()
+                val soc = SomeOtherClass(1.0,"one")
+                var sc2:SomeClass = SomeClass()
+                var soc2:SomeOtherClass = SomeOtherClass(2,"two")
                 var d:Double = 1.0
-                var f:Float = 1f
+                var f = 1f
+                var f2:Float = 1f
                 val i:Int = 1
+                val i2  = 1
                 val l:Long= 2L
                 val b:Boolean = true
+                if(true) {
+                    a=1.0
+                } else {
+                    a=2.0
+                }
             }
             """.trimIndent()
         val formattedCode =
             """
-            fun main(arg1:double, arg2:double) {
-                var d:double = 1.0
-                var f:double = 1f
-                val i:int = 1
-                val l:int= 2L
-                val b:bool = true
+            class SomeClass;
+            class SomeOtherClass(val a:Double, val b:String)
+
+            fun main(arg1:Double, arg2:Float,arg3:AClass) {
+                Double a
+                final sc = SomeClass()
+                final soc = SomeOtherClass(1.0,"one")
+                SomeClass sc2 = SomeClass()
+                SomeOtherClass soc2 = SomeOtherClass(2,"two")
+                Double d = 1.0
+                var f = 1f
+                Float f2 = 1f
+                final Int i = 1
+                final i2  = 1
+                final Long l= 2L
+                final Boolean b = true
+                if(true) {
+                    a=1.0
+                } else {
+                    a=2.0
+                }
             }
             """.trimIndent()
-        val rulesToTest= listOf<Rule>(BasicTypesNamesRule())
+        val rulesToTest= listOf<Rule>(VariableTypeBeforeNameRule())
         val actualFormattedCode = runRulesOnCodeFragment(code, rulesToTest)
         Assertions.assertThat(actualFormattedCode).isEqualTo(formattedCode)
     }
