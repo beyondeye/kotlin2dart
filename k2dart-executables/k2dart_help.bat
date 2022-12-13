fe06294b5e1fbbf585e226ca2c87ecc1aca1de15
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-13 12:20:46 +0200

    some small fix to bat files

--- k2dart-executables/k2dart_help.bat
+++ k2dart-executables/k2dart_help.bat
@@ -1,3 +1,3 @@
-rem to generate kt2dart.jar run the shadowJarExecutable task and get
+rem To generate kt2dart.jar run the shadowJarExecutable gradle task and then
 rem  copy \ktlint\build\libs\ktlint-0.48.0-SNAPSHOT-all.jar to this directory and rename to k2dart.jar
 java -jar .\k2dart.jar --help
