177ed2ca7c1266de370ab4632439a8e5e7303df8
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-15 15:58:29 +0200

    updated k2dart.bat with bug workaround

--- k2dart-executables/k2dart.bat
+++ k2dart-executables/k2dart.bat
@@ -1,3 +1,4 @@
 rem to generate kt2dart.jar run the shadowJarExecutable task and get
 rem  copy \ktlint\build\libs\ktlint-0.48.0-SNAPSHOT-all.jar to this directory and rename to k2dart.jar
-java -jar .\k2dart.jar
+rem the add-opens flag is required as workaround of issue https://github.com/pinterest/ktlint/issues/1391
+java --add-opens=java.base/java.lang=ALL-UNNAMED -jar .\k2dart.jar
