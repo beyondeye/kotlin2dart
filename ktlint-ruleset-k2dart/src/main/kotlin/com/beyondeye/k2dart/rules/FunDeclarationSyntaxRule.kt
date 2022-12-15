763a545131ecb32541a17e499f10dbaca1663697
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-15 12:07:29 +0200

    progress in implementing FunDeclarationSyntaxRule.kt

--- ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/rules/FunDeclarationSyntaxRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/beyondeye/k2dart/rules/FunDeclarationSyntaxRule.kt
@@ -1,13 +1,14 @@
 package com.beyondeye.k2dart.rules
 
-import com.beyondeye.k2dart.utils.asDartNode
+import com.beyondeye.k2dart.k2dartRulesetId
 import com.beyondeye.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
-import com.pinterest.ktlint.core.ast.ElementType
-import com.pinterest.ktlint.core.ast.nextCodeSibling
+import com.pinterest.ktlint.core.ast.*
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.PsiTypeElementImpl
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
+import org.jetbrains.kotlin.psi.KtTypeReference
 
 
 public class FunDeclarationSyntaxRule : Rule("$k2dartRulesetId:$ruleName") {
@@ -19,17 +20,104 @@ public class FunDeclarationSyntaxRule : Rule("$k2dartRulesetId:$ruleName") {
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
-        if (node.isDartNode()) return
-        //  ~.psi.KtProperty (PROPERTY)
-        //    ~.c.i.p.impl.source.tree.LeafPsiElement (VAR_KEYWORD) "var"
+
+        //  ~.psi.KtNamedFunction (FUN)
+        //    ~.c.i.p.impl.source.tree.LeafPsiElement (FUN_KEYWORD) "fun"
         //    ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
-        //    ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "d"
+        //    ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "fn1"
+        //    ~.psi.KtParameterList (VALUE_PARAMETER_LIST)
+        //      ~.c.i.p.impl.source.tree.LeafPsiElement (LPAR) "("
+        //      ~.psi.KtParameter (VALUE_PARAMETER)
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "arg1"
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
+        //        ~.psi.KtTypeReference (TYPE_REFERENCE)
+        //          ~.psi.KtUserType (USER_TYPE)
+        //            ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
+        //              ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "Double"
+        //      ~.c.i.p.impl.source.tree.LeafPsiElement (COMMA) ","
+        //      ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //      ~.psi.KtParameter (VALUE_PARAMETER)
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "arg2"
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
+        //        ~.psi.KtTypeReference (TYPE_REFERENCE)
+        //          ~.psi.KtUserType (USER_TYPE)
+        //            ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
+        //              ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "Float"
+        //      ~.c.i.p.impl.source.tree.LeafPsiElement (COMMA) ","
+        //      ~.psi.KtParameter (VALUE_PARAMETER)
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "arg3"
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
+        //        ~.psi.KtTypeReference (TYPE_REFERENCE)
+        //          ~.psi.KtUserType (USER_TYPE)
+        //            ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
+        //              ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "AClass"
+        //      ~.c.i.p.impl.source.tree.LeafPsiElement (RPAR) ")"
         //    ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
         //    ~.psi.KtTypeReference (TYPE_REFERENCE)
         //      ~.psi.KtUserType (USER_TYPE)
         //        ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
         //          ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "Double"
-        if(node.elementType != ElementType.PROPERTY) return
+        if (node izNot ElementType.FUN) return
+
+        if (node.isDartNode()) return
+        //find the "fun" keyword node, we will substitute it with return type specification
+        var funKeywordNode =node.firstChildNode ?: return
+        if(funKeywordNode izNot ElementType.FUN) {
+            funKeywordNode= funKeywordNode.nextSibling { it iz ElementType.FUN_KEYWORD } ?: return
+        }
+
+        val parameterListNode=funKeywordNode.nextSibling { it iz ElementType.VALUE_PARAMETER_LIST } ?:return
+        //TODO instead of colonNode it is possible to have an = node that must be substituted with =>
+        var isEqualSyntax=false
+        var isUnitReturn=false
+        val colonAfterParamsNode = parameterListNode.nextSibling { it iz ElementType.COLON }
+        var returnTypeNode:ASTNode?=null
+        if(colonAfterParamsNode==null) {
+            val equalAfterParamsNode = parameterListNode.nextSibling { it iz ElementType.EQ }
+            if(equalAfterParamsNode!=null) {
+                isEqualSyntax=true
+                equalAfterParamsNode  as LeafPsiElement?
+                equalAfterParamsNode?.replaceWithText("=>")
+            } else {
+                isUnitReturn=true
+                val parentNode=funKeywordNode.treeParent
+//                parentNode.addChild("void",funKeywordNode ) //void type node
+                parentNode.removeChild(funKeywordNode)
+            }
+        } else {
+            returnTypeNode=colonAfterParamsNode.nextSibling { it iz ElementType.TYPE_REFERENCE }
+            //parentNode.addChild(returnTypeNode,funKeywordNode ) //void type node
+            //parentNode.removeChild(funKeywordNode)
+
+        }
+
+        var nextParam=parameterListNode.firstChildNode
+        while(true) {
+            nextParam=nextParam.nextSibling { it iz ElementType.VALUE_PARAMETER }
+            if(nextParam==null) break
+            val identifierNode=nextParam.findChildByType(ElementType.IDENTIFIER)
+            if(identifierNode==null) {
+                //todo this should not happen
+                throw NotImplementedError()
+            }
+            val colonNode=identifierNode!!.nextCodeSibling()
+            if(colonNode==null) {
+                //todo this should not happen
+                throw NotImplementedError()
+            }
+            val typeNode = colonNode!!.nextSibling { it iz ElementType.TYPE_REFERENCE }
+            if(typeNode==null) {
+                //TODO this should not happen
+                throw NotImplementedError()
+            }
+            val parentNode=nextParam.treeParent //we actually already have this
+            parentNode.removeChild(colonNode)
+            parentNode.removeChild(typeNode)
+            parentNode.addChild(typeNode,identifierNode) //add typeNode before identifierNode
+            parentNode.addChild(PsiWhiteSpaceImpl(" "),identifierNode) //add space between typespec and identifier
+        }
+
+        /*
         var varvalKeywordNode=node.firstChildNode
         var isVar=true
         if(varvalKeywordNode.elementType==ElementType.VAL_KEYWORD) {
@@ -72,5 +160,10 @@ public class FunDeclarationSyntaxRule : Rule("$k2dartRulesetId:$ruleName") {
             return
         }
     }
+         */
+    }
+//    fun createTypeNode() :KtTypeReference {
+//        PsiTypeElementImpl
+//    }
 }
 
--- ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/FunDeclarationSyntaxRuleTest.kt
+++ ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/FunDeclarationSyntaxRuleTest.kt
@@ -5,57 +5,20 @@ import com.pinterest.ktlint.core.Rule
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
+//
 class FunDeclarationSyntaxRuleTest {
     @Test
     fun `change syntax for function declaration`() {
         val code =
             """
-            class SomeClass;
-            class SomeOtherClass(val a:Double, val b:String)
-
-            fun main(arg1:Double, arg2:Float,arg3:AClass) {
-                var a:Double
-                val sc = SomeClass()
-                val soc = SomeOtherClass(1.0,"one")
-                var sc2:SomeClass = SomeClass()
-                var soc2:SomeOtherClass = SomeOtherClass(2,"two")
-                var d:Double = 1.0
-                var f = 1f
-                var f2:Float = 1f
-                val i:Int = 1
-                val i2  = 1
-                val l:Long= 2L
-                val b:Boolean = true
-                if(true) {
-                    a=1.0
-                } else {
-                    a=2.0
-                }
+            fun fn1(arg1:Double, arg2:Float,arg3:AClass):Double {
+                return 1.0
             }
             """.trimIndent()
         val formattedCode =
             """
-            class SomeClass;
-            class SomeOtherClass(val a:Double, val b:String)
-
-            fun main(arg1:Double, arg2:Float,arg3:AClass) {
-                Double a
-                final sc = SomeClass()
-                final soc = SomeOtherClass(1.0,"one")
-                SomeClass sc2 = SomeClass()
-                SomeOtherClass soc2 = SomeOtherClass(2,"two")
-                Double d = 1.0
-                var f = 1f
-                Float f2 = 1f
-                final Int i = 1
-                final i2  = 1
-                final Long l= 2L
-                final Boolean b = true
-                if(true) {
-                    a=1.0
-                } else {
-                    a=2.0
-                }
+            Double fn1(Double arg1,Float arg2,AClass arg3) {
+                return 1.0
             }
             """.trimIndent()
         val rulesToTest= listOf<Rule>(VariableTypeBeforeNameRule())
