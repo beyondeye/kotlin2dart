36394cba4ae6e727548b8a37b9bd2bd3245c25ca
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-15 11:51:33 +0200

    removed git subcommands from ktlint command line args

--- ktlint/src/main/kotlin/com/pinterest/ktlint/Main.kt
+++ ktlint/src/main/kotlin/com/pinterest/ktlint/Main.kt
@@ -3,8 +3,6 @@
 package com.pinterest.ktlint
 
 import com.pinterest.ktlint.internal.GenerateEditorConfigSubCommand
-import com.pinterest.ktlint.internal.GitPreCommitHookSubCommand
-import com.pinterest.ktlint.internal.GitPrePushHookSubCommand
 import com.pinterest.ktlint.internal.KtlintCommandLine
 import com.pinterest.ktlint.internal.PrintASTSubCommand
 import com.pinterest.ktlint.internal.printCommandLineHelpOrVersionUsage
@@ -13,8 +11,6 @@ import picocli.CommandLine
 public fun main(args: Array<String>) {
     val ktlintCommand = KtlintCommandLine()
     val commandLine = CommandLine(ktlintCommand)
-        .addSubcommand(GitPreCommitHookSubCommand.COMMAND_NAME, GitPreCommitHookSubCommand())
-        .addSubcommand(GitPrePushHookSubCommand.COMMAND_NAME, GitPrePushHookSubCommand())
         .addSubcommand(PrintASTSubCommand.COMMAND_NAME, PrintASTSubCommand())
         .addSubcommand(GenerateEditorConfigSubCommand.COMMAND_NAME, GenerateEditorConfigSubCommand())
     val parseResult = commandLine.parseArgs(*args)
@@ -33,8 +29,6 @@ private fun handleSubCommand(
     parseResult: CommandLine.ParseResult,
 ) {
     when (val subCommand = parseResult.subcommand().commandSpec().userObject()) {
-        is GitPreCommitHookSubCommand -> subCommand.run()
-        is GitPrePushHookSubCommand -> subCommand.run()
         is PrintASTSubCommand -> subCommand.run()
         is GenerateEditorConfigSubCommand -> subCommand.run()
         else -> commandLine.usage(System.out, CommandLine.Help.Ansi.OFF)
