07ab1a59328ff64c5b1626694601238b6087712d
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-20 09:58:33 +0200

    fixed added run_k2dart_on_test_code.bat

--- run_k2dart_on_test_code.bat
+++ run_k2dart_on_test_code.bat
@@ -2,12 +2,10 @@ rem rebuild ktlint jar file
 rem .\gradlew.bat shadowJarExecutable
 
 cd .\k2dart-executables\
-rem make sure to first build ktlint by running the gradle task shadowJarExecutable ( see the batch file build_k2dart.bat)
-rem now copy the ktlint.jar to this directory and rename it k2dart.jar
-copy .\ktlint\build\libs\ktlint-0.48.0-SNAPSHOT-all.jar .\k2dart.jar
+rem update k2dart.jar to latest built version
+copy ..\ktlint\build\libs\ktlint-0.48.0-SNAPSHOT-all.jar .\k2dart.jar
 
-rem to generate kt2dart.jar run the shadowJarExecutable task and get
-rem  copy \ktlint\build\libs\ktlint-0.48.0-SNAPSHOT-all.jar to this directory and rename to k2dart.jar
+rem run k2dart on code in \k2dart-executables\test_code directory
 rem the add-opens flag is required as workaround of issue https://github.com/pinterest/ktlint/issues/1391
 java --add-opens=java.base/java.lang=ALL-UNNAMED -jar .\k2dart.jar
 
