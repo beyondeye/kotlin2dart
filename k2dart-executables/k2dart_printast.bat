cd3c26f2fb0ff104446d14b88a134222f792e44a
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-26 16:04:39 +0200

    k2dart_printast.bat now take an argument with the name of the file to print the ast for

--- k2dart-executables/k2dart_printast.bat
+++ k2dart-executables/k2dart_printast.bat
@@ -1,3 +1,21 @@
+rem pass to this script a single parameter with the name of the file you want to generate the AST for
+rem For example .\test_code\basic\comments.kts will match the file test_code\basic\comments.kts
 rem to generate kt2dart.jar run the shadowJarExecutable task and get
 rem  copy \ktlint\build\libs\ktlint-0.48.0-SNAPSHOT-all.jar to this directory and rename to k2dart.jar
-java -jar .\k2dart.jar --print-ast  "test_code\classes\class_pri**.kts" >.\ast.txt
+@echo off
+rem see https://stackoverflow.com/questions/26551/how-can-i-pass-arguments-to-a-batch-file
+rem see https://stackoverflow.com/questions/15567809/batch-extract-path-and-filename-from-a-variable
+rem see http://w3schools.org.in/setlocal.htm
+setlocal
+
+set arg1=%1
+
+FOR %%i IN ("%arg1%") DO (
+set filedrive=%%~di
+set filepath=%%~pi
+set filename=%%~ni
+set fileextension=%%~xi
+)
+
+@echo on
+java -jar .\k2dart.jar --print-ast  "%arg1%" >"%filedrive%%filepath%%filename%_ast.txt"
