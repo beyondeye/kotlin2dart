9bd4b313075c20242e8ea664724a959c6e7692f5
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-28 11:09:46 +0200

    added visibility_modifiers.dart to test_code

---
+++ k2dart-executables/test_code/basic/visibility_modifiers.dart
@@ -0,0 +1,21 @@
+/* private */ val a=1;
+ /* protected */
+
+ void someProtectedFun() {
+    print("hi");
+}
+ /* internal */
+ class A {
+     /* private */
+     val a=1;
+     /* protected */
+     void fn() {
+        print("hello");
+    }
+}
+ /* public */
+
+ class B {
+     /* internal */
+     var b=0;
+}
