e2af076e04745f03a31297ec34869a66f4f1598e
Dario Elyasy <dario-e@beyond-eye.com>
2022-12-29 09:42:53 +0200

    BitOpsRule and test implementation completed

--- ktlint-ruleset-k2dart/src/main/kotlin/com/pinterest/ktlint/ruleset/k2dart/rules/BitOpsRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/pinterest/ktlint/ruleset/k2dart/rules/BitOpsRule.kt
@@ -1,16 +1,14 @@
 package com.pinterest.ktlint.ruleset.k2dart.rules
 
 import com.pinterest.ktlint.ruleset.k2dart.k2dartRulesetId
-import com.pinterest.ktlint.ruleset.k2dart.utils.asDartNode
 import com.pinterest.ktlint.ruleset.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType
-import com.pinterest.ktlint.core.ast.iz
+import com.pinterest.ktlint.core.ast.izNot
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 
-// FinalInsteadOfValRule this rule is obsolete, since it has been integrated in VariableTypeBeforeNameRule
-// but we keep it here because we use it as template for generating new rules
+// //todo the .inv() op is not yet translated
 public class BitOpsRule : Rule("$k2dartRulesetId:$ruleName") {
     public companion object {
         public const val ruleName:String="bits-ops"
@@ -21,11 +19,25 @@ public class BitOpsRule : Rule("$k2dartRulesetId:$ruleName") {
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
         if(node.isDartNode()) return
-        if (node iz ElementType.VAL_KEYWORD) {
-            //*DARIO* emit is useless in k2dart, unless we want to write some log of what is corrected (useful for debugging)
-            // emit(node.startOffset, ruleName, true)
-            val newNode=(node as LeafElement).replaceWithText("final")  //update the AST with the converted keyword
-            newNode.asDartNode() //set also the dart node flag so that we will avoid processing this node again
-        }
+        if (node izNot ElementType.OPERATION_REFERENCE) return
+        /*
+            ~.psi.KtOperationReferenceExpression (OPERATION_REFERENCE)
+              ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "xor"
+        */
+        val opIdentifierNode=node.firstChildNode ?: return
+        if( opIdentifierNode !is LeafPsiElement) return
+        val newOpName= when(opIdentifierNode.text) {
+            "xor" -> "^"
+            "and" -> "&"
+            "or" -> "|"
+            "shl" -> "<<"
+            "shr" -> ">>"
+            "ushr" -> ">>>"
+            else ->null
+        } ?: return
+
+        opIdentifierNode.replaceWithText(newOpName)
+
+        //todo the .inv() op is not yet translated
     }
 }
--- ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/BitOpsRuleTest.kt
+++ ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/BitOpsRuleTest.kt
@@ -11,30 +11,30 @@ class BitOpsRuleTest {
     fun `change val keyword to final keyword`() {
         val code =
             """
-fun main()
-{
-    val a=8 shl 2
-    val b=8 shr 2
-    val c=8 ushr 3
-    val d=8 and 8
-    val e=8 or 4
-    val f=8 xor 2
-    val g=inv(8)
-}
-""".trimIndent()
+            fun main()
+            {
+                val a=8 shl 2
+                val b=8 shr 2
+                val c=8 ushr 3
+                val d=8 and 8
+                val e=8 or 4
+                val f=8 xor 2
+                val g=8.inv()
+            }
+            """.trimIndent()
         val formattedCode =
             """
-fun main()
-{
-    val a=8 shl 2
-    val b=8 shr 2
-    val c=8 ushr 3
-    val d=8 and 8
-    val e=8 or 4
-    val f=8 xor 2
-    val g=inv(8)
-}
-""".trimIndent()
+            fun main()
+            {
+                val a=8 << 2
+                val b=8 >> 2
+                val c=8 >>> 3
+                val d=8 & 8
+                val e=8 | 4
+                val f=8 ^ 2
+                val g=8.inv()
+            }
+            """.trimIndent()
         val actualFormattedCode = runRulesOnCodeFragment(code, listOf(BitOpsRule()))
         Assertions.assertThat(actualFormattedCode).isEqualTo(formattedCode)
     }
