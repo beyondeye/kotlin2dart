f01091880d521abcd6b9c6b854d077ad1f275409
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-02 08:51:34 +0200

    added skeleton of CompanionObjectRule.kt + test

---
+++ ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/CompanionObjectRuleTest.kt
@@ -0,0 +1,44 @@
+package com.beyondeye.k2dart
+
+import com.pinterest.ktlint.ruleset.k2dart.rules.CompanionObjectRule
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+// *DARIO* this for collecting result of a lint of operation
+
+class CompanionObjectRuleTest {
+    @Test
+    fun `companion object members transformed into static members`() {
+        val code =
+            """
+class A
+{
+    val a=1;
+    fun someFun():Int {return 1}
+    companion object {
+        const val someConst:Int=123
+        private fun someStaticFun():Int { return  1 }
+        fun someOtherStaticFun() = "hello"
+        var someVariable:String="userid"
+    }
+}
+""".trimIndent()
+        val formattedCode =
+            """
+class A
+{
+    val a=1;
+    fun someFun():Int {return 1}
+    companion object {
+        const val someConst:Int=123
+        private fun someStaticFun():Int { return  1 }
+        fun someOtherStaticFun() = "hello"
+        var someVariable:String="userid"
+    }
+}
+""".trimIndent()
+        val actualFormattedCode = runRulesOnCodeFragment(code, listOf(CompanionObjectRule()))
+        Assertions.assertThat(actualFormattedCode).isEqualTo(formattedCode)
+    }
+
+
+}
