4a25805322aa53a6232de0faf1685e68deb66db4
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-02 08:41:35 +0200

    added test code for companion_object.kts + ast

---
+++ k2dart-executables/test_code/classes/companion_object.kts
@@ -0,0 +1,11 @@
+class A
+{
+    val a=1;
+    fun someFun():Int {return 1}
+    companion object {
+        const val someConst:Int=123
+        private fun someStaticFun():Int { return  1 }
+        fun someOtherStaticFun() = "hello"
+        var someVariable:String="userid"
+    }
+}
