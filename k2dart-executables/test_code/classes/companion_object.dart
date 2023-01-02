d1e735ba44a1ee1dd47b057b8d4bc93ddfbf17e1
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-02 12:59:35 +0200

    added companion_object.dart to test code

---
+++ k2dart-executables/test_code/classes/companion_object.dart
@@ -0,0 +1,17 @@
+class A
+{
+    final a=1;
+    int someFun() {return 1;}
+    static const final int someConst=123;
+
+     /* private */    static  int someStaticFun() { return  1; }
+    static fun someOtherStaticFun() => "hello"
+    static String someVariable="userid";
+
+    companion object {
+        
+        
+        
+        
+    }
+}
