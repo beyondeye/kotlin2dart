7f181080e8f18ca3b9b72d277bd3d09c3cbefc6b
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-03 15:38:38 +0200

    added math_ops test code + ast

---
+++ k2dart-executables/test_code/stdlib/math_ops.kts
@@ -0,0 +1,14 @@
+import kotlin.math.ceil
+import kotlin.math.round
+import kotlin.math.abs
+
+fun main() {
+    val a1= Math.round(1.2)
+    val a2 = round(1.2)
+
+    val b1= Math.ceil(2.3)
+    val b2= ceil(2.3)
+
+    val c1= Math.abs(3.4)
+    val c2=abs(3.4)
+}
