8033eb0af91aa8b2088ddaa5e529bceb8886e24a
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-03 15:21:27 +0200

    added is_empty.dart to test_code

---
+++ k2dart-executables/test_code/stdlib/is_empty.dart
@@ -0,0 +1,11 @@
+
+void main() {
+    final a="hi   ";
+    final a_is_empty=a.isEmpty;
+    final b="";
+    final b_not_empty=b.isNotEmpty;
+
+    final vec= listOf(1,2,3);
+    final vec_is_empty=vec.isEmpty;
+    final vec_not_empty=vec.isNotEmpty;
+}
