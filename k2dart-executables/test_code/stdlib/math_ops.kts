3ec83b85b1f62327d148cb39ac781f935afe0091
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-03 15:53:34 +0200

    updated math_ops.kts test code and associated ast

--- k2dart-executables/test_code/stdlib/math_ops.kts
+++ k2dart-executables/test_code/stdlib/math_ops.kts
@@ -1,10 +1,15 @@
 import kotlin.math.ceil
 import kotlin.math.round
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 fun main() {
     val a1= Math.round(1.2)
     val a2 = round(1.2)
+    val a3 = c2.roundToInt()
+    val a4 = (1.2+c2).roundToInt()
+    val a5 = Math.abs(1.0+(1.2+c2).roundToInt())
+    val a6 = (1.0+(1.2+c2).roundToInt())/2.0
 
     val b1= Math.ceil(2.3)
     val b2= ceil(2.3)
