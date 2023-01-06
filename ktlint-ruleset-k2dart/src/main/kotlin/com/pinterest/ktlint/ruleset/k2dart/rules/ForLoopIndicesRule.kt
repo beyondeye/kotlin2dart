aaf1185c76ce63582aef7f6c22a18d2ac6cb8438
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-06 11:59:21 +0200

    ForLoopIndicesRule.kt impl + test completed

--- ktlint-ruleset-k2dart/src/main/kotlin/com/pinterest/ktlint/ruleset/k2dart/rules/ForLoopIndicesRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/pinterest/ktlint/ruleset/k2dart/rules/ForLoopIndicesRule.kt
@@ -1,18 +1,20 @@
 package com.pinterest.ktlint.ruleset.k2dart.rules
 
 import com.pinterest.ktlint.ruleset.k2dart.k2dartRulesetId
-import com.pinterest.ktlint.ruleset.k2dart.utils.asDartNode
 import com.pinterest.ktlint.ruleset.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
-import com.pinterest.ktlint.core.ast.ElementType
-import com.pinterest.ktlint.core.ast.iz
+import com.pinterest.ktlint.core.ast.*
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 
 // convert Kotlin for loop in the equivalent Dart constructs
 public class ForLoopIndicesRule : Rule("$k2dartRulesetId:$ruleName") {
     public companion object {
         public const val ruleName:String="for-loop-indices"
+        private const val dotdotOp=".."
+        private const val downToOp="downTo"
+        private const val untilOp="until"
+        private const val stepOp="step"
     }
     override fun beforeVisitChildNodes(
         node: ASTNode,
@@ -20,11 +22,99 @@ public class ForLoopIndicesRule : Rule("$k2dartRulesetId:$ruleName") {
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
         if(node.isDartNode()) return
-        if (node iz ElementType.VAL_KEYWORD) {
-            //*DARIO* emit is useless in k2dart, unless we want to write some log of what is corrected (useful for debugging)
-            // emit(node.startOffset, ruleName, true)
-            val newNode=(node as LeafElement).replaceWithText("final")  //update the AST with the converted keyword
-            newNode.asDartNode() //set also the dart node flag so that we will avoid processing this node again
+        if(node.izNot( ElementType.FOR)) return
+        val forNode=node
+        //      ~.psi.KtForExpression (FOR)
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (FOR_KEYWORD) "for"
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (LPAR) "("
+        //        ~.psi.KtParameter (VALUE_PARAMETER)
+        //          ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "i"
+        //        ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (IN_KEYWORD) "in"
+        //        ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+
+        //    ~.psi.KtForExpression (FOR)
+        //      ~.c.i.p.impl.source.tree.LeafPsiElement (FOR_KEYWORD) "for"
+        //      ~.c.i.p.impl.source.tree.LeafPsiElement (LPAR) "("
+        //      ~.psi.KtParameter (VALUE_PARAMETER)
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "i"
+        //        ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
+        //        ~.psi.KtTypeReference (TYPE_REFERENCE)
+        //          ~.psi.KtUserType (USER_TYPE)
+        //            ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
+        //              ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "Int"
+        val valueParamNode=forNode.findChildByType(ElementType.VALUE_PARAMETER) ?: return
+        val identifierNode=valueParamNode.findChildByType(ElementType.IDENTIFIER) ?: return
+        //this can be null
+        val loopIndexType=identifierNode.nextSibling { it iz ElementType.TYPE_REFERENCE }
+
+        val loopRangeNode = valueParamNode.nextSibling { it iz ElementType.LOOP_RANGE } ?: return
+
+        //     ~.psi.KtContainerNode (LOOP_RANGE)
+        //       ~.psi.KtBinaryExpression (BINARY_EXPRESSION)
+        //         ~.psi.KtConstantExpression (INTEGER_CONSTANT)
+        //           ~.c.i.p.impl.source.tree.LeafPsiElement (INTEGER_LITERAL) "0"
+        //         ~.psi.KtOperationReferenceExpression (OPERATION_REFERENCE)
+        //           ~.c.i.p.impl.source.tree.LeafPsiElement (RANGE) ".."
+        //         ~.psi.KtConstantExpression (INTEGER_CONSTANT)
+        //           ~.c.i.p.impl.source.tree.LeafPsiElement (INTEGER_LITERAL) "3"
+        var loopRangeBinarExprNode=loopRangeNode.firstChildNode
+        if(loopRangeBinarExprNode izNot ElementType.BINARY_EXPRESSION) return
+
+        //       ~.psi.KtBinaryExpression (BINARY_EXPRESSION)
+        //         ~.psi.KtBinaryExpression (BINARY_EXPRESSION)
+        //           ~.psi.KtConstantExpression (INTEGER_CONSTANT)
+        //             ~.c.i.p.impl.source.tree.LeafPsiElement (INTEGER_LITERAL) "6"
+        //           ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //           ~.psi.KtOperationReferenceExpression (OPERATION_REFERENCE)
+        //             ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "downTo"
+        //           ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //           ~.psi.KtConstantExpression (INTEGER_CONSTANT)
+        //             ~.c.i.p.impl.source.tree.LeafPsiElement (INTEGER_LITERAL) "1"
+        //         ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //         ~.psi.KtOperationReferenceExpression (OPERATION_REFERENCE)
+        //           ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "step"
+        //         ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+        //         ~.psi.KtConstantExpression (INTEGER_CONSTANT)
+        //           ~.c.i.p.impl.source.tree.LeafPsiElement (INTEGER_LITERAL) "2"
+        var opReferenceNode= loopRangeBinarExprNode.findChildByType(ElementType.OPERATION_REFERENCE) ?: return
+        val stepOpReferenceNode:ASTNode?
+        var stepBinaryExprNode:ASTNode? = null
+        var stepValueNode:ASTNode? = null
+        if(opReferenceNode.text== stepOp) { //we have a step, the LOOP_RANGE AST structure has an additional level (a binary expr in a binary expr)
+            stepBinaryExprNode=loopRangeBinarExprNode
+            stepOpReferenceNode=opReferenceNode
+            stepValueNode = stepOpReferenceNode.nextCodeSibling() ?:return
+            loopRangeBinarExprNode = loopRangeBinarExprNode.firstChildNode
+            opReferenceNode = loopRangeBinarExprNode.findChildByType(ElementType.OPERATION_REFERENCE) ?: return
         }
+        val opTxt=opReferenceNode.text
+        if(opTxt!= dotdotOp && opTxt!= downToOp && opTxt!= untilOp) return //unsupported type of loop
+        val loopStartNode=opReferenceNode.prevCodeSibling() ?:return
+        val loopEndNode=opReferenceNode.nextCodeSibling() ?: return
+        val isDecreasingIndexLoop=opTxt== downToOp
+        val idTxt=identifierNode.text
+        val stepNodeTxt=stepValueNode?.text
+        val dartStepTxt:String=
+            if (stepNodeTxt == null) {
+                if (!isDecreasingIndexLoop) "$idTxt++" else "$idTxt--"
+            } else { //stepNodeTxt!=null
+                if (!isDecreasingIndexLoop) "$idTxt+=$stepNodeTxt" else "$idTxt-=$stepNodeTxt"
+            }
+        val dartLoopInitTxt="var $idTxt=${loopStartNode.text}"
+
+        val loopEndTxt = loopEndNode.text
+        val dartLimitTxt: String = if (isDecreasingIndexLoop) {
+            "$idTxt>=$loopEndTxt"
+        } else { //not decreasing
+            if (opTxt == dotdotOp) {
+                "$idTxt<=$loopEndTxt"
+            } else "$idTxt<$loopEndTxt" //until
+        }
+
+        val dartLoopFullText="for ( $dartLoopInitTxt; $dartLimitTxt; $dartStepTxt ) "
+        val bodyNode=forNode.findChildByType(ElementType.BODY) ?:return
+        forNode.removeRange(forNode.firstChildNode,bodyNode) //bodyNode is not included in range
+        forNode.addChild(LeafPsiElement(ElementType.DART_CODE,dartLoopFullText),bodyNode)
     }
 }
--- ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/ForLoopIndicesRuleTest.kt
+++ ktlint-ruleset-k2dart/src/test/kotlin/com/beyondeye/k2dart/ForLoopIndicesRuleTest.kt
@@ -10,78 +10,82 @@ class ForLoopIndicesRuleTest {
     fun `for loop converted to dart syntax`() {
         val code =
             """
-fun main()
-{
-    val i=-1
-    println(i) //-1
-    for(i in 0..3) { // 0 1 2 3
-        println(i)
-    }
-    println(i) //-1
+            fun main()
+            {
+                for(i in 0 .. 3) println(i)
 
-    for(i:Int in 0 until 3) { // 0 1 2
-        println(i)
-    }
+                val i=-1
+                println(i) //-1
+                for(i in 0..3) { // 0 1 2 3
+                    println(i)
+                }
+                println(i) //-1
 
-    for(i:Int in 3 downTo 1) { // 3 2 1
-        println(i)
-    }
+                for(i:Int in 0 until 3) { // 0 1 2
+                    println(i)
+                }
 
-    for(i:Int in 6 downTo 1 step 2) { //6 4 2
-        println(i)
-    }
+                for(i:Int in 3 downTo 1) { // 3 2 1
+                    println(i)
+                }
 
-    for(i in 0..3 step 2) { // 0 2
-        println(i)
-    }
+                for(i:Int in 6 downTo 1 step 2) { //6 4 2
+                    println(i)
+                }
 
-    val array = listOf(0,11,22,33)
-    for (i in array.indices) { //0 11 22 33
-        println(array[i])
-    }
+                for(i in 0..3 step 2) { // 0 2
+                    println(i)
+                }
 
-    for ((index, value) in array.withIndex()) { //0 11 22 33
-        println("the element at ${"$"}index is ${"$"}value")
-    }
-}
-""".trimIndent()
+                val array = listOf(0,11,22,33)
+                for (i in array.indices) { //0 11 22 33
+                    println(array[i])
+                }
+
+                for ((index, value) in array.withIndex()) { //0 11 22 33
+                    println("the element at ${"$"}index is ${"$"}value")
+                }
+            }
+            """.trimIndent()
         val formattedCode =
             """
-fun main()
-{
-    val i=-1
-    println(i) //-1
-    for(i in 0..3) { // 0 1 2 3
-        println(i)
-    }
-    println(i) //-1
+            fun main()
+            {
+                for ( var i=0; i<=3; i++ ) println(i)
 
-    for(i:Int in 0 until 3) { // 0 1 2
-        println(i)
-    }
+                val i=-1
+                println(i) //-1
+                for ( var i=0; i<=3; i++ ) { // 0 1 2 3
+                    println(i)
+                }
+                println(i) //-1
 
-    for(i:Int in 3 downTo 1) { // 3 2 1
-        println(i)
-    }
+                for ( var i=0; i<3; i++ ) { // 0 1 2
+                    println(i)
+                }
 
-    for(i:Int in 6 downTo 1 step 2) { //6 4 2
-        println(i)
-    }
+                for ( var i=3; i>=1; i-- ) { // 3 2 1
+                    println(i)
+                }
 
-    for(i in 0..3 step 2) { // 0 2
-        println(i)
-    }
+                for ( var i=6; i>=1; i-=2 ) { //6 4 2
+                    println(i)
+                }
 
-    val array = listOf(0,11,22,33)
-    for (i in array.indices) { //0 11 22 33
-        println(array[i])
-    }
+                for ( var i=0; i<=3; i+=2 ) { // 0 2
+                    println(i)
+                }
 
-    for ((index, value) in array.withIndex()) { //0 11 22 33
-        println("the element at ${"$"}index is ${"$"}value")
-    }
-}
-""".trimIndent()
+                val array = listOf(0,11,22,33)
+                for (i in array.indices) { //0 11 22 33
+                    println(array[i])
+                }
+
+                for ((index, value) in array.withIndex()) { //0 11 22 33
+                    println("the element at ${"$"}index is ${"$"}value")
+                }
+            }
+            """.trimIndent()
         val actualFormattedCode = runRulesOnCodeFragment(code, listOf(ForLoopIndicesRule()))
         Assertions.assertThat(actualFormattedCode).isEqualTo(formattedCode)
     }
