e6e01bfffcbf7a5b698d451399d1fb46601e2b1d
Dario Elyasy <dario-e@beyond-eye.com>
2023-01-08 09:42:20 +0200

    progress in impl of GetterAndSetterRule.kt

--- ktlint-ruleset-k2dart/src/main/kotlin/com/pinterest/ktlint/ruleset/k2dart/rules/GetterAndSetterRule.kt
+++ ktlint-ruleset-k2dart/src/main/kotlin/com/pinterest/ktlint/ruleset/k2dart/rules/GetterAndSetterRule.kt
@@ -1,13 +1,11 @@
 package com.pinterest.ktlint.ruleset.k2dart.rules
 
 import com.pinterest.ktlint.ruleset.k2dart.k2dartRulesetId
-import com.pinterest.ktlint.ruleset.k2dart.utils.asDartNode
 import com.pinterest.ktlint.ruleset.k2dart.utils.isDartNode
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType
-import com.pinterest.ktlint.core.ast.iz
+import com.pinterest.ktlint.core.ast.izNot
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
 
 // FinalInsteadOfValRule this rule is obsolete, since it has been integrated in VariableTypeBeforeNameRule
 // but we keep it here because we use it as template for generating new rules
@@ -21,11 +19,41 @@ public class GetterAndSetterRule : Rule("$k2dartRulesetId:$ruleName") {
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
         if(node.isDartNode()) return
-        if (node iz ElementType.VAL_KEYWORD) {
-            //*DARIO* emit is useless in k2dart, unless we want to write some log of what is corrected (useful for debugging)
-            // emit(node.startOffset, ruleName, true)
-            val newNode=(node as LeafElement).replaceWithText("final")  //update the AST with the converted keyword
-            newNode.asDartNode() //set also the dart node flag so that we will avoid processing this node again
-        }
+        if (node izNot  ElementType.PROPERTY_ACCESSOR) return
+        val propAccessorNode=node
+        val getKeywordNode=(propAccessorNode.findChildByType(ElementType.GET_KEYWORD))
+        val setKeywordNode=(propAccessorNode.findChildByType(ElementType.SET_KEYWORD))
+        getKeywordNode?.let { handleGetter(propAccessorNode,it) }
+        setKeywordNode?.let { handleSetter(propAccessorNode,it) }
+    }
+
+   //    ~.psi.KtProperty (PROPERTY)
+   //      ~.c.i.p.impl.source.tree.LeafPsiElement (VAL_KEYWORD) "val"
+   //      ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+   //      ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "b"
+   //      ~.c.i.p.impl.source.tree.LeafPsiElement (COLON) ":"
+   //      ~.psi.KtTypeReference (TYPE_REFERENCE)
+   //        ~.psi.KtUserType (USER_TYPE)
+   //          ~.psi.KtNameReferenceExpression (REFERENCE_EXPRESSION)
+   //            ~.c.i.p.impl.source.tree.LeafPsiElement (IDENTIFIER) "String"
+   //      ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+   //      ~.psi.KtPropertyAccessor (PROPERTY_ACCESSOR)
+   //        ~.c.i.p.impl.source.tree.LeafPsiElement (GET_KEYWORD) "get"
+   //        ~.c.i.p.impl.source.tree.LeafPsiElement (LPAR) "("
+   //        ~.c.i.p.impl.source.tree.LeafPsiElement (RPAR) ")"
+   //        ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+   //        ~.c.i.p.impl.source.tree.LeafPsiElement (EQ) "="
+   //        ~.c.i.p.impl.source.tree.PsiWhiteSpaceImpl (WHITE_SPACE) " "
+   //        ~.psi.KtStringTemplateExpression (STRING_TEMPLATE)
+   //          ~.c.i.p.impl.source.tree.LeafPsiElement (OPEN_QUOTE) """
+   //          ~.psi.KtLiteralStringTemplateEntry (LITERAL_STRING_TEMPLATE_ENTRY)
+   //            ~.c.i.p.impl.source.tree.LeafPsiElement (REGULAR_STRING_PART) "Hello"
+   //          ~.c.i.p.impl.source.tree.LeafPsiElement (CLOSING_QUOTE) """
+    private fun handleGetter(propAccessorNode: ASTNode, getKeywordNode: ASTNode) {
+        val propertyNode=propAccessorNode.treeParent ?:return
+    }
+
+    private fun handleSetter(propAccessorNode: ASTNode, setKeywordNode: ASTNode) {
+        val propertyNode=propAccessorNode.treeParent ?:return
     }
 }
